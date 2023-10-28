61707499735d267ee065effd2f02f0077f8d91a3
Sankar <1890648+srgsanky@users.noreply.github.com>
2023-10-28 14:23:29 -0700

    Initial Commit

---
+++ SafariTabsBackup/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SafariTabsBackup
+//
+//  Created by Sankar Shanmugam on 10/28/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
